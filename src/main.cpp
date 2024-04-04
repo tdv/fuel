aa636b921a1d2161ab520b7ea5fe6fecbd6b91d8
tdv <tkachenkodmitryv@gmail.com>
2024-04-05 01:15:36 +0300

    A little experiment with ResNet101-coco

--- src/main.cpp
+++ src/main.cpp
@@ -1,393 +1,88 @@
-// STD
-#include <chrono>
-#include <cstdlib>
 #include <iostream>
-#include <thread>
-#include <vector>
+#include <opencv2/opencv.hpp>
 
-// OpenCV
-#include <opencv2/core.hpp>
-#include <opencv2/dnn.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-
-namespace fuel
+cv::Mat imageToBlob(cv::Mat const image)
 {
-
-struct object
-    : public std::enable_shared_from_this<object>
-{
-    virtual ~object() = default;
-};
-
-using object_ptr = std::shared_ptr<object>;
-
-};
-
-namespace fuel
-{
-
-using mat = cv::Mat;
-
-struct source
-    : public object
-{
-    virtual mat get() const = 0;
-};
-
-using source_ptr = std::shared_ptr<source>;
-
-}   // namespace fuel
+    if (image.empty())
+        throw std::runtime_error{"Failed to read image."};
 
 
-namespace fuel
-{
-namespace impl
-{
+    cv::Mat resized_image;
+    cv::resize(image, resized_image, cv::Size(224, 224), 0, 0, cv::INTER_LINEAR);
 
-class web_cam_exception
-    : public std::runtime_error
-{
-public:
-    web_cam_exception(std::string const &message)
-        : std::runtime_error{message}
-    {
-    }
-};
+    cv::cvtColor(resized_image, resized_image, cv::COLOR_BGR2RGB);
 
-class web_cam
-    : public source
-{
-public:
-    explicit web_cam(std::string const &cam_name)
-        : cam_name_{cam_name}
-        , cap_{cam_name_}
-    {
-        if (!cap_.isOpened())
-        {
-            throw web_cam_exception{"Failed to open webcam \"" +
-                    cam_name_ + "\"."};
-        }
+    cv::Scalar mean = cv::Scalar(0.485, 0.456, 0.406);
+    cv::Scalar stddev = cv::Scalar(0.229, 0.224, 0.225);
+    resized_image.convertTo(resized_image, CV_32F, 1.0 / 255.0);
+    resized_image -= mean;
+    resized_image /= stddev;
 
-        std::this_thread::sleep_for(start_delay_);
-    }
+    cv::Mat blob = cv::dnn::blobFromImage(resized_image);
 
-private:
-    std::chrono::milliseconds const start_delay_{500};
+    std::cout << "Shape of the input blob: " << blob.size << std::endl;
 
-    std::string cam_name_;
-    mutable cv::VideoCapture cap_;
-
-    // source
-    virtual mat get() const override final
-    {
-        mat img;
-
-        if (!cap_.read(img))
-        {
-            throw web_cam_exception{"Failed to read frame from webcam \"" +
-                    cam_name_ + "\"."};
-        }
-
-        mat res;
-        cv::flip(img, res, 1);
-
-        return res;
-    }
-};
-}   // namespace impl
-
-source_ptr make_web_cam_source(std::string const &cam_name)
-{
-    return std::make_shared<impl::web_cam>(cam_name);
+    return blob;
 }
 
-}   // namespace fuel
+cv::Mat extractPersonMask(cv::Mat const& output_blob, int image_height, int image_width) {
+    // Reshape the output blob to have dimensions [21 x 224 x 224]
+    cv::Mat reshaped_blob = output_blob.reshape(1, 21);
 
+    // Extract the 15th channel (index 14, 0-based indexing)
+    cv::Mat person_channel = reshaped_blob.row(20);
 
-namespace fuel
-{
+    // Reshape the channel to match the spatial dimensions of the input image
+    person_channel = person_channel.reshape(1, image_height);
 
-struct processor
-    : public object
-{
-    virtual mat process(mat img) = 0;
-};
+    // Apply thresholding
+    cv::Mat person_mask;
+    cv::threshold(person_channel, person_mask, 2.0, 255, cv::THRESH_BINARY_INV);
 
-using processor_ptr = std::shared_ptr<processor>;
+    // Resize the mask to match the spatial dimensions of the input image
+    cv::resize(person_mask, person_mask, cv::Size(image_width, image_height));
 
-}   // namespace fuel
-
-namespace fuel
-{
-namespace impl
-{
-
-class denoising_processor
-    : public processor
-{
-public:
-    explicit denoising_processor(int ksize)
-        : ksize_{ksize}
-    {
-        if (ksize_ < 3)
-            ksize_ = 3;
-        if (!(ksize_ % 2))
-            ++ksize_;
-    }
-
-private:
-    int ksize_;
-
-    virtual mat process(mat img) override final
-    {
-        mat res;
-
-        cv::medianBlur(img, res, ksize_);
-
-        return res;
-    }
-};
-
-}   // namespace impl
-
-processor_ptr make_denoising_processor(int ksize = 3)
-{
-    return std::make_shared<impl::denoising_processor>(ksize);
+    return person_mask;
 }
 
-}   // namespace fuel
+void test_resnet_101_coco(std::string const &model_path,
+                     std::string const &image_path)
+{
+    auto resnet = cv::dnn::readNetFromONNX(model_path);
+    resnet.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
+    resnet.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA);
 
-namespace fuel
-{
-namespace impl
-{
+    auto img = cv::imread(image_path);
 
-class auto_correct_processor
-    : public processor
-{
-public:
-private:
-    virtual mat process(mat img) override final
+    auto blob = imageToBlob(img);
+    resnet.setInput(blob);
+    auto output = resnet.forward();
+    std::cout << "Shape of the output blob: " << output.size << std::endl;
+
+    auto mask = extractPersonMask(output, 224, 224);
+    std::cout << "Shape of the mask: " << mask.size << std::endl;
+    //std::cout << "Mask: " << mask << std::endl;
+
+    cv::Mat resized_image;
+    cv::resize(img, resized_image, cv::Size(224, 224), 0, 0, cv::INTER_LINEAR);
+    while (cv::waitKey(100) != 27)
     {
-        //https://answers.opencv.org/question/75510/how-to-make-auto-adjustmentsbrightness-and-contrast-for-image-android-opencv-image-correction/
-
-        mat res;
-
-        BrightnessAndContrastAuto(img, res, 0);
-
-        return res;
+        cv::imshow("Source", resized_image);
+        cv::imshow("Mask", mask);
     }
-
-    //----------------
-
-    void BrightnessAndContrastAuto(const cv::Mat &src, cv::Mat &dst, float clipHistPercent=0)
-    {
-
-        CV_Assert(clipHistPercent >= 0);
-        CV_Assert((src.type() == CV_8UC1) || (src.type() == CV_8UC3) || (src.type() == CV_8UC4));
-
-        int histSize = 256;
-        float alpha, beta;
-        double minGray = 0, maxGray = 0;
-
-        //to calculate grayscale histogram
-        cv::Mat gray;
-        if (src.type() == CV_8UC1) gray = src;
-        else if (src.type() == CV_8UC3) cvtColor(src, gray, cv::COLOR_BGR2GRAY);
-        else if (src.type() == CV_8UC4) cvtColor(src, gray, cv::COLOR_BGRA2GRAY);
-        if (clipHistPercent == 0)
-        {
-            // keep full available range
-            cv::minMaxLoc(gray, &minGray, &maxGray);
-        }
-        else
-        {
-            cv::Mat hist; //the grayscale histogram
-
-            float range[] = { 0, 256 };
-            const float* histRange = { range };
-            bool uniform = true;
-            bool accumulate = false;
-            calcHist(&gray, 1, 0, cv::Mat (), hist, 1, &histSize, &histRange, uniform, accumulate);
-
-            // calculate cumulative distribution from the histogram
-            std::vector<float> accumulator(histSize);
-            accumulator[0] = hist.at<float>(0);
-            for (int i = 1; i < histSize; i++)
-            {
-                accumulator[i] = accumulator[i - 1] + hist.at<float>(i);
-            }
-
-            // locate points that cuts at required value
-            float max = accumulator.back();
-            clipHistPercent *= (max / 100.0); //make percent as absolute
-            clipHistPercent /= 2.0; // left and right wings
-            // locate left cut
-            minGray = 0;
-            while (accumulator[minGray] < clipHistPercent)
-                minGray++;
-
-            // locate right cut
-            maxGray = histSize - 1;
-            while (accumulator[maxGray] >= (max - clipHistPercent))
-                maxGray--;
-        }
-
-        // current range
-        float inputRange = maxGray - minGray;
-
-        alpha = (histSize - 1) / inputRange;   // alpha expands current range to histsize range
-        beta = -minGray * alpha;             // beta shifts current range so that minGray will go to 0
-
-        // Apply brightness and contrast normalization
-        // convertTo operates with saurate_cast
-        src.convertTo(dst, -1, alpha, beta);
-
-        // restore alpha channel from source
-        if (dst.type() == CV_8UC4)
-        {
-            int from_to[] = { 3, 3};
-            cv::mixChannels(&src, 4, &dst,1, from_to, 1);
-        }
-        return;
-    }
-};
-
-}   // namespace impl
-
-processor_ptr make_auto_correct_processor()
-{
-    return std::make_shared<impl::auto_correct_processor>();
 }
 
-}   // namespace fuel
-
-namespace fuel
-{
-namespace impl
-{
-
-class background_processor
-    : public processor
-{
-public:
-    background_processor(std::string const &new_bg_path,
-            std::string const &model_path,
-            std::string const &out_layer_name)
-        : out_layer_name_{out_layer_name}
-    {
-        net_ = cv::dnn::readNet(model_path);
-
-        net_.setPreferableBackend(cv::dnn::DNN_BACKEND_INFERENCE_ENGINE);
-        net_.setPreferableTarget(cv::dnn::DNN_TARGET_OPENCL);
-
-    }
-
-private:
-    cv::Size const input_size_{256, 256};
-
-    std::string out_layer_name_;
-
-    cv::dnn::Net net_;
-
-    virtual mat process(mat img) override final
-    {
-        auto const h = img.size[0];
-        auto const w = img.size[1];
-        auto const original_size = cv::Size{w, h};
-
-        auto blob = cv::dnn::blobFromImage(img, 1.0/255, input_size_,
-                cv::Scalar(), false, false, CV_32F);
-
-        net_.setInput(blob);
-
-        auto output = net_.forward(out_layer_name_);
-        auto mask = output.reshape(1, 256) > 0.5;
-
-        mat resized_mask;
-        cv::resize(mask, resized_mask, original_size, cv::INTER_LINEAR);
-
-        mat res(img.size(), img.type(), cv::Scalar(70, 70, 70));
-        //img.copyTo(res, resized_mask);
-
-        cv::bitwise_and(img, img, res, resized_mask);
-
-        cv::dilate(res, res, cv::Mat(), cv::Point(-1, -1), 1);
-        cv::erode(res, res, cv::Mat(), cv::Point(-1, -1), 1);
-
-        return res;
-    }
-};
-
-}   // namespace impl
-
-processor_ptr make_background_processor(std::string const &new_bg_path,
-        std::string const &model_path,
-        std::string const &out_layer_name = "")
-{
-    return std::make_shared<impl::background_processor>(new_bg_path,
-            model_path, out_layer_name);
-}
-
-}   // namespace fuel
-
-bool areEqual(const cv::Mat& a, const cv::Mat& b)
-{
-    cv::Mat temp;
-    cv::bitwise_xor(a,b,temp);
-    return !(cv::countNonZero(temp.reshape(1)));
-}
 
 int main()
 {
-    try
-    {
-        //cv::blendLinear()
-        auto const web_cam_name = "/dev/video0";
-
-        auto src = fuel::make_web_cam_source(web_cam_name);
-
-        auto const model_path = "../models/segmentation/openvino/model_float32.pb";
-        auto const bg_path = "";
-
-        std::vector<fuel::processor_ptr> processors{
-            fuel::make_denoising_processor(3),
-            fuel::make_auto_correct_processor(),
-            fuel::make_background_processor(bg_path, model_path, "")
-
-        };
-
-        fuel::mat prev;
-
-        while (true)
-        {
-
-            auto img = src->get();
-
-            if (prev.empty())
-                prev = img;
-
-            if (areEqual(prev, img))
-                continue;
-
-            prev = img;
-
-            for (auto &processor : processors)
-                img = processor->process(std::move(img));
-
-
-            cv::imshow("Image", img);
-
-            if (cv::waitKey(10) == 32)
-                break;
-        }
+    try {
+        auto const *model_path = "/media/dmitry/stg1/Tst/test_ltorch_bg_remove/resnet101/resnet101.onnx";
+        auto const *image_path = "../data/src.jpg";
+        test_resnet_101_coco(model_path, image_path);
     }
-    catch (std::exception const &e)
-    {
+    catch (std::exception const &e) {
         std::cerr << "An error has occured. Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
